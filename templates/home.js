074ff81ffbce93cb253d189bc6d8c62f8afcc1dc
Suhag Ahmed <89881464+suhag10@users.noreply.github.com>
2023-08-06 16:48:32 +0600

    templates

--- templates/home.js
+++
@@ -1,8 +0,0 @@
-// <------------------------------------------>
-// < printout appDiv                          >
-// <------------------------------------------>
-const appDiv = document.getElementById('app');
-appDiv.innerHTML = uiHeaderArea();
-appDiv.innerHTML += uiHomePageDiv();
-appDiv.innerHTML += uiTestimonialDiv();
-appDiv.innerHTML += uiFooterDiv();
