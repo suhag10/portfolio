9e047dbcbee4b6cf2c219c6b67bf9aedb33f5869
suhag10 <suhag78641@gmail.com>
2024-11-10 09:19:50 +0600

    Dev v2

---
+++ draft/data-design.js
@@ -0,0 +1,54 @@
+// Blog data
+export const dataBlog = {
+    1: {
+        date       : '',
+        category   : '',
+        thumbnail  : '',
+        title      : '',
+        content    : (`
+            <h2 class="font-sans text-4xl mb-20"></h3>
+            <p class="text-base mb-50 border-b border-slate-200 pb-50"></p>
+            <div class="row mb-50">
+                <h3 class="text-3xl w-4/12"></h3>
+                <p class="text-base w-8/12"></p>
+            </div>
+            <p class="text-base"></p>
+            <img src="" alt="" class="my-50">
+            <p class="text-base"></p>
+            <blockquote class="text-center my-50 border border-slate-400 p-30 w-8/12 mx-auto">
+                <div>
+                    <span class="text-3xl text-slate-200 inline-block mb-10">
+                        <svg width="34" height="28" viewBox="0 0 34 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M25.6645 0C27.2731 0 28.5892 0.329033 29.6129 0.987098C30.6366 1.64516 31.4774 2.48602 32.1355 3.50968C32.9398 4.67957 33.4516 5.95915 33.671 7.34839C33.8903 8.73764 34 9.87097 34 10.7484C34 14.3312 33.086 17.585 31.2581 20.5097C29.4301 23.4344 26.5785 25.8108 22.7032 27.6387L21.7161 25.6645C23.9828 24.714 25.9204 23.2151 27.529 21.1677C29.2108 19.1204 30.0516 17.0366 30.0516 14.9161C30.0516 14.0387 29.9419 13.271 29.7226 12.6129C28.5527 13.5634 27.2 14.0387 25.6645 14.0387C23.7634 14.0387 22.1183 13.4172 20.729 12.1742C19.3398 10.9312 18.6452 9.21291 18.6452 7.01936C18.6452 4.97205 19.3398 3.29033 20.729 1.9742C22.1183 0.658065 23.7634 0 25.6645 0ZM7.01936 0C8.62796 0 9.94409 0.329033 10.9677 0.987098C11.9914 1.64516 12.8323 2.48602 13.4903 3.50968C14.2946 4.67957 14.8065 5.95915 15.0258 7.34839C15.2452 8.73764 15.3548 9.87097 15.3548 10.7484C15.3548 14.3312 14.4409 17.585 12.6129 20.5097C10.7849 23.4344 7.93333 25.8108 4.05806 27.6387L3.07097 25.6645C5.33763 24.714 7.27527 23.2151 8.88387 21.1677C10.5656 19.1204 11.4065 17.0366 11.4065 14.9161C11.4065 14.0387 11.2968 13.271 11.0774 12.6129C9.90753 13.5634 8.55484 14.0387 7.01936 14.0387C5.11828 14.0387 3.47312 13.4172 2.08387 12.1742C0.694624 10.9312 0 9.21291 0 7.01936C0 4.97205 0.694624 3.29033 2.08387 1.9742C3.47312 0.658065 5.11828 0 7.01936 0Z" fill="currentColor"></path>
+                        </svg>                                                   
+                    </span>
+                </div>
+                <h4 class="font-normal">“Try”</h4>
+            </blockquote>
+            <p class="text-base"></p>
+            <ul class="list-disc ml-15 my-20">
+                <li class="text-base text-heading font-medium"></li>
+                <li class="text-base text-heading font-medium"></li>
+                <li class="text-base text-heading font-medium"></li>
+            </ul>
+            <p class="text-base"></p>
+        `),
+    },
+    2: {
+        date       : 'October 11, 2024',
+        category   : 'JavaScript',
+        thumbnail  : '',
+        title      : 'JavaScript Cheatsheets',
+        content    : '<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>' + `
+            <h3 class="text-rose-600">General</h3>
+            <pre class="language-js line-numbers code-toolbar">
+                <code></code>
+            </pre>
+            <h3 class="text-rose-600">Variables</h3>
+            <pre class="language-js line-numbers code-toolbar">
+                <code></code>
+            </pre>
+            <h3 class="text-rose-600">Strings</h3>
+        `,
+    },
+}
